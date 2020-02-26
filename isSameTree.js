1da1f6befae7ba8f6851da6cd7e72e41fc163139
dyaroshevych <dyaroshevych@gmail.com>
2020-02-27 00:47:22 +0300

    added isSameTree.js (problem 100)

---
+++ isSameTree.js
@@ -0,0 +1,20 @@
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+console.log(
+  isSameTree(
+    {
+      val: 1,
+      right: { val: 3, right: null, left: null },
+      left: { val: 2, right: null, left: null }
+    },
+    {
+      val: 1,
+      right: { val: 3, right: null, left: null },
+      left: { val: 2, right: null, left: null }
+    }
+  )
+);
