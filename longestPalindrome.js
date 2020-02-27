b7a55089c93ec0e76dbc8634a7e43f96752fb2d4
dyaroshevych <dyaroshevych@gmail.com>
2020-02-27 12:29:31 +0300

    added solution to problem 5

---
+++ longestPalindrome.js
@@ -0,0 +1,36 @@
+const longestPalindrome = str => {
+  let maxLength = 0,
+    maxStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let pointerOdd = 1,
+      pointerEven = 0;
+    while (
+      str[i - pointerOdd] === str[i + pointerOdd] &&
+      str[i - pointerOdd] &&
+      str[i + pointerOdd]
+    ) {
+      pointerOdd++;
+    }
+    while (
+      str[i - pointerEven] === str[i + pointerEven + 1] &&
+      str[i - pointerEven] &&
+      str[i + pointerEven + 1]
+    ) {
+      pointerEven++;
+    }
+    let currentLengthOdd = 2 * pointerOdd - 1;
+    let currentLengthEven = 2 * pointerEven;
+    if (maxLength < currentLengthOdd) {
+      maxLength = currentLengthOdd;
+      maxStr = str.slice(i - pointerOdd + 1, i + pointerOdd);
+    }
+    if (maxLength < currentLengthEven) {
+      maxLength = currentLengthEven;
+      maxStr = str.slice(i - pointerEven + 1, i + pointerEven + 1);
+    }
+  }
+
+  return maxStr;
+};
+
+console.log(longestPalindrome("abbad"));
