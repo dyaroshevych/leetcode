41c705de1a8face2dc3cba9dfb3a1b1940c2c408
dyaroshevych <dyaroshevych@gmail.com>
2020-02-27 13:20:38 +0300

    added solution to problems 292 and 461

---
+++ canWinNim.js
@@ -0,0 +1,15 @@
+// DP solution
+const canWinNimDP = num => {
+  let arr = [true, true, true];
+
+  for (let i = 3; i < num; i++) {
+    arr.push(!arr[i - 1] || !arr[i - 2] || !arr[i - 3]);
+  }
+
+  return arr[num - 1];
+};
+
+// O(1) solution
+const canWinNim = num => !!(num % 4);
+
+console.log(canWinNim(5));
---
+++ hammingDistance.js
@@ -0,0 +1,27 @@
+const binaryFromNum = num => {
+  let binaryStr = "";
+  while (num > 0) {
+    binaryStr = (num % 2) + binaryStr;
+    num = Math.floor(num / 2);
+  }
+  return binaryStr;
+};
+
+const hammingDistance = (num1, num2) => {
+  const binary1 = binaryFromNum(num1),
+    binary2 = binaryFromNum(num2);
+
+  let maxLength = Math.max(binary1.length, binary2.length),
+    differentCount = 0;
+
+  for (let i = 0; i < maxLength; i++)
+    if (
+      !!Number(binary1[binary1.length - i - 1]) !==
+      !!Number(binary2[binary2.length - i - 1])
+    )
+      differentCount++;
+
+  return differentCount;
+};
+
+console.log(hammingDistance(1, 4));
