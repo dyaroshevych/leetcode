407891b151d9af47472aeb8679547588429723bc
dyaroshevych <dyaroshevych@gmail.com>
2020-03-13 13:25:24 +0300

    added solutions to problems 560 and 713

---
+++ 0560. Subarray Sum Equals K.js
@@ -0,0 +1,39 @@
+const subarraySumQuadraticTime = (nums, k) => {
+  const sums = [];
+  let currentSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sums.push(currentSum);
+    currentSum += nums[i];
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (sums[j] - sums[i] + nums[j] === k) result++;
+    }
+  }
+
+  return result;
+};
+
+const subarraySum = (nums, k) => {
+  const sums = {};
+
+  let currentSum = (result = 0);
+  for (let i = 0; i < nums.length; i++) {
+    if (sums[currentSum]) {
+      sums[currentSum]++;
+    } else {
+      sums[currentSum] = 1;
+    }
+    currentSum += nums[i];
+    if (sums[currentSum - k]) {
+      result += sums[currentSum - k];
+    }
+  }
+
+  return result;
+};
+
+console.log(subarraySum([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0));
---
+++ 0713. Subarray Product Less Than K.js
@@ -0,0 +1,17 @@
+const numSubarrayProductLessThanK = (nums, k) => {
+  if (k <= 1) return 0;
+  let left = (right = result = 0),
+    product = 1;
+
+  while (right < nums.length) {
+    product *= nums[right];
+
+    while (product >= k) product /= nums[left++];
+
+    result += ++right - left;
+  }
+
+  return result;
+};
+
+console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100));
