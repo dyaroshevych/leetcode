e3b65caaedb6ceb87077b70da911180642b7b8e2
dyaroshevych <dyaroshevych@gmail.com>
2020-03-20 01:59:21 +0400

    added a test case for problem 226

--- 0226. Invert Binary Tree.js
+++ 0226. Invert Binary Tree.js
@@ -1,3 +1,14 @@
+// INPUT: 4
+//      /   \
+//      2     7
+//     / \   / \
+//    1   3 6   9
+// OUTPUT: 4
+//       /   \
+//       7     2
+//      / \   / \
+//     9   6 3   1
+
 const invertTreeRecursive = root => {
   if (!root) return root;
 
