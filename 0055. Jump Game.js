b3321bc7dd241b48b083035aae2d7a928f92700f
dyaroshevych <dyaroshevych@gmail.com>
2020-04-01 00:20:40 +0300

    added solution to problem 55

---
+++ 0055. Jump Game.js
@@ -0,0 +1,11 @@
+const canJump = nums => {
+  let maxJumpIdx = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    maxJumpIdx = Math.max(maxJumpIdx, i + nums[i]);
+    if (maxJumpIdx <= i) return false;
+  }
+
+  return true;
+};
+
+console.log(canJump([3, 2, 1, 0, 4]));
