585f6e6dae2a579916d35e9db7e5fcb138a40c73
dyaroshevych <dyaroshevych@gmail.com>
2020-04-02 10:07:24 +0300

    added a better solution to problem 136

--- 0136. Single Number.js
+++ 0136. Single Number.js
@@ -1,7 +1,7 @@
 // INPUT: [9, 5, 4, 3, 4, 1, 5, 3, 9, 8, 8]
 // OUTPUT: 1
 
-const singleNumber = arr => {
+const singleNumberHashSet = arr => {
   const elements = new Set();
   for (let i = 0; i < arr.length; i++) {
     if (elements.has(arr[i])) {
@@ -11,6 +11,15 @@ const singleNumber = arr => {
     }
   }
   return elements.values().next().value;
+};
+
+const singleNumber = arr => {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result ^= arr[i];
+  }
+
+  return result;
 };
 
 console.log(singleNumber([4, 1, 2, 1, 2]));
