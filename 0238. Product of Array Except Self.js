80b32e71acfcdddd6f9f5f539d09f53e0b7bb688
dyaroshevych <dyaroshevych@gmail.com>
2020-04-03 10:54:55 +0300

    added a test case for problem 238

--- 0238. Product of Array Except Self.js
+++ 0238. Product of Array Except Self.js
@@ -1,20 +1,23 @@
-const productExceptSelf = nums => {
-  const leftProduct = new Array(nums.length);
+// INPUT: [ 1, 2, 3, 4 ]
+// OUTPUT: [ 24, 12, 8, 6 ]
 
-  leftProduct[0] = 1;
+const productExceptSelf = nums => {
+  const products = new Array(nums.length);
+
+  products[0] = 1;
 
   for (let i = 1; i < nums.length; i++) {
-    leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+    products[i] = products[i - 1] * nums[i - 1];
   }
 
   let rightProduct = 1;
 
   for (let i = nums.length - 1; i >= 0; i--) {
-    leftProduct[i] = leftProduct[i] * rightProduct;
+    products[i] = products[i] * rightProduct;
     rightProduct *= nums[i];
   }
 
-  return leftProduct;
+  return products;
 };
 
 console.log(productExceptSelf([1, 2, 3, 4]));
