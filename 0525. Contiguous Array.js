e3d3627808249ced02217ed41f543ed18d68df04
dyaroshevych <dyaroshevych@gmail.com>
2020-04-14 01:04:07 +0300

    added solution to problem 525

---
+++ 0525. Contiguous Array.js
@@ -0,0 +1,16 @@
+const findMaxLength = (nums) => {
+  const numsCount = { 0: -1 };
+  let count = (maxLength = 0);
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] === 0 ? -1 : 1;
+    if (numsCount[count] >= -1) {
+      maxLength = Math.max(maxLength, i - numsCount[count]);
+    } else {
+      numsCount[count] = i;
+    }
+  }
+
+  return maxLength;
+};
+
+console.log(findMaxLength([0, 1, 0, 1]));
