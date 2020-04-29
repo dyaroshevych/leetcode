5a65957c9a7100d16fc69338817c6f45d3f3421f
dyaroshevych <dyaroshevych@gmail.com>
2020-04-30 01:09:21 +0300

    add solution to problem 124

---
+++ 0124. Binary Tree Maximum Path Sum.js
@@ -0,0 +1,27 @@
+const maxPathSum = (root) => {
+  if (root === null) return 0;
+
+  let maxSum = root.val;
+
+  const getMaxPathSum = (node) => {
+    if (node === null) return 0;
+    const leftMaxSum = Math.max(getMaxPathSum(node.left), 0),
+      rightMaxSum = Math.max(getMaxPathSum(node.right), 0);
+
+    maxSum = Math.max(maxSum, node.val + leftMaxSum + rightMaxSum);
+
+    return node.val + Math.max(leftMaxSum, rightMaxSum);
+  };
+
+  getMaxPathSum(root);
+
+  return maxSum;
+};
+
+console.log(
+  maxPathSum({
+    val: 1,
+    left: { val: 2, left: null, right: null },
+    right: { val: 3, left: null, right: null },
+  })
+);
