62c76adbb86717ff028cc4e659c652cfb12b8cd4
dyaroshevych <dyaroshevych@gmail.com>
2020-06-03 23:47:55 +0300

    add 322 and 1029

---
+++ 0322. Coin Change.js
@@ -0,0 +1,18 @@
+const coinChange = (coins, amount) => {
+  const dp = new Array(amount + 1);
+
+  dp[0] = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    let min = Infinity;
+    for (let coin of coins) {
+      if (dp[i - coin] >= 0) min = Math.min(min, dp[i - coin] + 1);
+    }
+
+    dp[i] = min;
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
+
+console.log(coinChange([1, 2, 5], 11));
---
+++ 1029. Two City Scheduling.js
@@ -0,0 +1,19 @@
+const twoCitySchedCost = (costs) => {
+  costs.sort((a, b) => a[1] - a[0] - (b[1] - b[0]));
+
+  let total = 0;
+  for (let i = 0; i < costs.length / 2; i++) {
+    total += costs[i][1] + costs[costs.length - i - 1][0];
+  }
+
+  return total;
+};
+
+console.log(
+  twoCitySchedCost([
+    [10, 20],
+    [30, 200],
+    [400, 50],
+    [30, 20],
+  ])
+);
