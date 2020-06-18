837c5bc0f48ea61dd10ec744041bf708371fd167
dyaroshevych <dyaroshevych@gmail.com>
2020-06-19 00:33:01 +0300

    add a test case for 48

--- 0048. Rotate Image.js
+++ 0048. Rotate Image.js
@@ -1,4 +1,15 @@
-const rotate = matrix => {
+// INPUT:[
+//   [ 7, 4, 1 ],
+//   [ 8, 5, 2 ],
+//   [ 9, 6, 3 ]
+// ]
+// OUTPUT: [
+//   [ 1, 2, 3 ],
+//   [ 4, 5, 6 ],
+//   [ 7, 8, 9 ]
+// ]
+
+const rotate = (matrix) => {
   if (matrix.length === 0) return matrix;
 
   for (let i = 0; i < matrix.length / 2; i++) {
@@ -22,6 +33,6 @@ console.table(
   rotate([
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
   ])
 );
