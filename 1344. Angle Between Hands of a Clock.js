478140421a19dfff03407d6b67c7afc9cc7e7576
dyaroshevych <dyaroshevych@gmail.com>
2020-07-14 23:16:43 +0300

    add a test case for 1344

--- 1344. Angle Between Hands of a Clock.js
+++ 1344. Angle Between Hands of a Clock.js
@@ -1,3 +1,6 @@
+// INPUT: 3, 15
+// OUTPUT: 7.5
+
 const angleClock = (hour, minutes) => {
   const hourAngle = (hour * 30 + minutes / 2) % 360;
   const minutesAngle = minutes * 6;
@@ -6,3 +9,5 @@ const angleClock = (hour, minutes) => {
 
   return Math.min(angle, 360 - angle);
 };
+
+console.log(angleClock(3, 15));
