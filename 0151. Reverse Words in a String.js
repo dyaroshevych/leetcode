3585e4e76bffcf19ac726b54193304f24b30d52f
dyaroshevych <dyaroshevych@gmail.com>
2020-07-15 23:08:13 +0300

    add solution to 151

---
+++ 0151. Reverse Words in a String.js
@@ -0,0 +1,15 @@
+const reverseWords = (str) => {
+  str = str.trim();
+  const words = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let j = i;
+    while (j < str.length && str[j] !== " ") j++;
+    if (i < j) words.push(str.slice(i, j));
+    i = j;
+  }
+
+  return words.reverse().join(" ");
+};
+
+console.log(reverseWords(" hello! world "));
