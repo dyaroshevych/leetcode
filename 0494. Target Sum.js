0c76477cfadadb0eb76e87effe8e20643a3b2b2f
dyaroshevych <dyaroshevych@gmail.com>
2020-07-18 23:41:41 +0300

    improved the solution of 494 and added a test case

--- 0494. Target Sum.js
+++ 0494. Target Sum.js
@@ -1,3 +1,6 @@
+// INPUT: [ 1, 1, 1, 1, 1 ], 3
+// OUTPUT: 5
+
 const findTargetSumWaysRecursive = (nums, sum) => {
   let count = 0;
 
@@ -23,24 +26,19 @@ const findTargetSumWays = (nums, sum) => {
   const addNumber = (currSum, idx) => {
     if (idx === nums.length) return Number(currSum === sum);
 
-    if (memo[idx] && memo[idx][currSum]) return memo[idx][currSum];
+    if (memo[idx] && memo[idx][currSum] !== undefined)
+      return memo[idx][currSum];
 
     const resultsSum =
       addNumber(currSum + nums[idx], idx + 1) +
       addNumber(currSum - nums[idx], idx + 1);
 
-    if (resultsSum) {
-      if (memo[idx] && memo[idx][currSum]) memo[idx][currSum] += resultsSum;
-      else if (memo[idx]) memo[idx][currSum] = resultsSum;
-      else memo[idx] = { [currSum]: resultsSum };
+    if (memo[idx] && memo[idx][currSum]) memo[idx][currSum] += resultsSum;
+    else if (memo[idx]) memo[idx][currSum] = resultsSum;
+    else memo[idx] = { [currSum]: resultsSum };
 
-      return memo[idx][currSum];
-    }
-
-    return 0;
+    return resultsSum;
   };
-
-  addNumber(0, 0);
 
   return addNumber(0, 0);
 };
