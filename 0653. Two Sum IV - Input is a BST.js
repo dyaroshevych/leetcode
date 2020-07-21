13547f48b94c804ce19f52cb94ed8a34102f1ca1
dyaroshevych <dyaroshevych@gmail.com>
2020-07-21 00:28:41 -0600

    add a test case for 653

--- 0653. Two Sum IV - Input is a BST.js
+++ 0653. Two Sum IV - Input is a BST.js
@@ -1,3 +1,6 @@
+// INPUT: [ 5, 3, 6, 2, 4, null, 7 ], target: 9
+// OUTPUT: true
+
 const findTarget = (root, target) => {
   const nums = [];
 
@@ -29,9 +32,16 @@ const findTarget = (root, target) => {
 };
 
 console.log(
-  findTarget({
-    val: 2,
-    left: { val: 1, left: null, right: null },
-    right: { val: 3, left: null, right: null },
-  })
+  findTarget(
+    {
+      val: 5,
+      left: {
+        val: 3,
+        left: { val: 2, left: null, right: null },
+        right: { val: 4, left: null, right: null },
+      },
+      right: { val: 6, left: null, right: { val: 7, left: null, right: null } },
+    },
+    9
+  )
 );
