ba6c66aaa72cfce1031b5f94b00765934ee7a738
dyaroshevych <dyaroshevych@gmail.com>
2020-08-03 22:47:03 +0300

    add a test case for 1035

--- 1035. Uncrossed Lines.js
+++ 1035. Uncrossed Lines.js
@@ -1,22 +1,31 @@
+// INPUT: [ 2, 1 ], [ 1, 2, 1, 3, 3, 2 ]
+// OUTPUT: 2
+
+const getLinesCount = (dp, lineA, lineB, i, j) => {
+  if (i >= lineA.length || j >= lineB.length) return 0;
+
+  if (dp[i][j] !== -1) return dp[i][j];
+
+  if (lineA[i] === lineB[j]) {
+    dp[i][j] = 1 + getLinesCount(dp, lineA, lineB, i + 1, j + 1);
+  } else {
+    dp[i][j] = Math.max(
+      getLinesCount(dp, lineA, lineB, i + 1, j),
+      getLinesCount(dp, lineA, lineB, i, j + 1)
+    );
+  }
+
+  return dp[i][j];
+};
+
 const maxUncrossedLines = (A, B) => {
-  const dp = new Array(A.length)
-    .fill(0)
-    .map((element) => new Array(B.length).fill(-1));
+  const dp = new Array(A.length);
 
-  const getLinesCount = (i, j) => {
-    if (i >= A.length || j >= B.length) return 0;
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(B.length).fill(-1);
+  }
 
-    if (dp[i][j] !== -1) return dp[i][j];
-
-    if (A[i] === B[j]) return (dp[i][j] = 1 + getLinesCount(i + 1, j + 1));
-
-    return (dp[i][j] = Math.max(
-      getLinesCount(i + 1, j),
-      getLinesCount(i, j + 1)
-    ));
-  };
-
-  return getLinesCount(0, 0);
+  return getLinesCount(dp, A, B, 0, 0);
 };
 
 console.log(maxUncrossedLines([2, 1], [1, 2, 1, 3, 3, 2]));
