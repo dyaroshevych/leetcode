27683a3c05a5359fb5319b50af3818e03d6fba2c
dyaroshevych <dyaroshevych@gmail.com>
2020-08-07 20:45:51 +0300

    add a test case for 617

--- 0617. Merge Two Binary Trees.js
+++ 0617. Merge Two Binary Trees.js
@@ -1,21 +1,32 @@
+// INPUT: 1           2
+//       / \         / \
+//      3   2       1   3
+//     /             \   \
+//    5               4   7
+// OUTPUT: 3
+//        / \
+//       4   5
+//      / \   \
+//     5   4   7
+
 const mergeTrees = (root1, root2) => {
   if (!root1) return root2;
-
-  const mergeNodes = (node1, node2) => {
-    if (!node2) return;
-
-    node1.val += node2.val;
-
-    if (!node1.left) node1.left = node2.left;
-    else mergeNodes(node1.left, node2.left);
-
-    if (!node1.right) node1.right = node2.right;
-    else mergeNodes(node1.right, node2.right);
-  };
 
   mergeNodes(root1, root2);
 
   return root1;
+};
+
+const mergeNodes = (node1, node2) => {
+  if (!node2) return;
+
+  node1.val += node2.val;
+
+  if (!node1.left) node1.left = node2.left;
+  else mergeNodes(node1.left, node2.left);
+
+  if (!node1.right) node1.right = node2.right;
+  else mergeNodes(node1.right, node2.right);
 };
 
 console.log(
