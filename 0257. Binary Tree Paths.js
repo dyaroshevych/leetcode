b7567fc8f477d5c08e1ff73b8775e1d1262ef13c
dyaroshevych <dyaroshevych@gmail.com>
2020-08-08 22:09:46 +0300

    add a test case for 257

--- 0257. Binary Tree Paths.js
+++ 0257. Binary Tree Paths.js
@@ -1,3 +1,10 @@
+// INPUT: 1
+//       / \
+//      2   3
+//       \
+//        5
+// OUTPUT: [ '1->2->5', '1->3' ]
+
 const binaryTreePaths = (root) => {
   if (root === null) {
     return [];
@@ -24,3 +31,23 @@ const helper = (node, currPath, paths) => {
 
   return paths;
 };
+
+console.log(
+  binaryTreePaths({
+    val: 1,
+    left: {
+      val: 2,
+      left: null,
+      right: {
+        val: 5,
+        left: null,
+        right: null,
+      },
+    },
+    right: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+  })
+);
