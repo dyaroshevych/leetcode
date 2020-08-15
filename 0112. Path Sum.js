61bdb65e33fd5fda7ff2403d8d29fe41ac786136
dyaroshevych <dyaroshevych@gmail.com>
2020-08-15 22:12:19 +0300

    add a test case for 112

--- 0112. Path Sum.js
+++ 0112. Path Sum.js
@@ -1,3 +1,13 @@
+// INPUT: 5
+//       / \
+//      4   8
+//     /   / \
+//    11  13  4
+//   /  \      \
+//  7    2      1
+// target: 22
+// OUTPUT: true
+
 const hasPathSum = (root, target) => helper(root, 0, target);
 
 const helper = (node, currSum, target) => {
@@ -13,3 +23,46 @@ const helper = (node, currSum, target) => {
     helper(node.right, currSum, target)
   );
 };
+
+console.log(
+  hasPathSum(
+    {
+      val: 5,
+      left: {
+        val: 4,
+        left: {
+          val: 11,
+          left: {
+            val: 7,
+            left: null,
+            right: null,
+          },
+          right: {
+            val: 2,
+            left: null,
+            right: null,
+          },
+        },
+        right: null,
+      },
+      right: {
+        val: 8,
+        left: {
+          val: 23,
+          left: null,
+          right: null,
+        },
+        right: {
+          val: 4,
+          left: null,
+          right: {
+            val: 1,
+            left: null,
+            right: null,
+          },
+        },
+      },
+    },
+    22
+  )
+);
