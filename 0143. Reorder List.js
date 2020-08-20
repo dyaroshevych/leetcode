22955e97e16ce1a3ebf83ab7dfb6ce1a81faa3e9
dyaroshevych <dyaroshevych@gmail.com>
2020-08-20 22:16:53 +0300

    add solution to 143

---
+++ 0143. Reorder List.js
@@ -0,0 +1,55 @@
+const reorderList = (head) => {
+  if (head === null || head.next === null) {
+    return;
+  }
+
+  let p1 = head;
+  let p2 = head.next;
+
+  // find the middle of the list
+  while (p2.next && p2.next.next) {
+    p1 = p1.next;
+    p2 = p2.next.next;
+  }
+
+  let prev = null;
+  let curr = p1.next;
+
+  // reverse second half of the list
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  p1.next = prev;
+
+  // reorder the list
+  let first = head;
+  let second = prev;
+
+  while (second) {
+    const firstNext = first.next;
+    first.next = second;
+    first = firstNext;
+
+    const secondNext = second.next;
+    if (firstNext === prev) {
+      break;
+    }
+    second.next = firstNext;
+    second = secondNext;
+  }
+};
+
+const list = {
+  val: 1,
+  next: {
+    val: 2,
+    next: { val: 3, next: { val: 4, next: null } },
+  },
+};
+
+reorderList(list);
+console.log(list);
