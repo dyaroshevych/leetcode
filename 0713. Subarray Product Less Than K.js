484d1d616dd46ffa73e3f18a77e26156b59a7c7d
dyaroshevych <dyaroshevych@gmail.com>
2020-09-28 22:56:08 +0300

    provide a more elegant solution for 713

--- 0713. Subarray Product Less Than K.js
+++ 0713. Subarray Product Less Than K.js
@@ -1,20 +1,26 @@
 // INPUT: [ 10, 5, 2, 6 ], 100
 // OUTPUT: 8
 
-const numSubarrayProductLessThanK = (nums, k) => {
-  if (k <= 1) return 0;
-  let left = (right = result = 0),
-    product = 1;
-
-  while (right < nums.length) {
-    product *= nums[right];
-
-    while (product >= k) product /= nums[left++];
-
-    result += ++right - left;
+const numSubarrayProductLessThanK = (nums, K) => {
+  if (K <= 1) {
+    return 0;
   }
 
-  return result;
+  let start = 0;
+  let count = 0;
+  let currProduct = 1;
+
+  for (let end = 0; end < nums.length; end++) {
+    currProduct *= nums[end];
+
+    while (currProduct >= K) {
+      currProduct /= nums[start++];
+    }
+
+    count += end - start + 1;
+  }
+
+  return count;
 };
 
 console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100));
