d9b0dec857e98ec29922b34f148449ab9ca59375
dyaroshevych <dyaroshevych@gmail.com>
2020-10-19 16:33:35 +0300

    separate solutions by programming language

--- microsoft/lexicograficallySmallestString.js
+++
@@ -1,9 +0,0 @@
-const smallestString = str => {
-  let idx = 0;
-
-  while (idx < str.length && str[idx] < str[idx + 1]) idx++;
-
-  return str.slice(0, idx) + str.slice(idx + 1);
-};
-
-console.log(smallestString("abczd"));
